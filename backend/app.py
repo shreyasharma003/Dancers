a3be32e46c70c9237817e0d29f07472129004fc5
shreyasharma003 <shreyusharma11@gmail.com>
2025-12-21 08:25:17 +0530

    A Backend base project committed

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,10 @@ from flask import Flask
 from models import engine, session, Base, Dancer, DanceStyle, User
 from routes import register_routes
 from auth import register_auth_routes
+from flask_cors import CORS 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
