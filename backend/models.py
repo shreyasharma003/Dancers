01152fd33b9a942c5661879e0ed17e4b832493e9
shreyasharma003 <shreyusharma11@gmail.com>
2025-12-22 15:49:14 +0530

    models.py

--- backend/models.py
+++ backend/models.py
@@ -3,9 +3,10 @@ from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 import os
 
 # LOCAL_DATABASE_URL = "postgresql+psycopg2://postgres:Shreya03@localhost:5432/dancers_db"
-RENDER_DATABASE_URL = "postgresql://ssha:ZateStW6ucyfgA2JXeHrwCWwVP2P7Ezq@dpg-d54fquili9vc73efqtug-a/dancers_db_3tqd"
+# RENDER_DATABASE_URL = "postgresql://ssha:ZateStW6ucyfgA2JXeHrwCWwVP2P7Ezq@dpg-d54fquili9vc73efqtug-a/dancers_db_3tqd"
+SUPA_DATABASE_URL = "postgresql+psycopg2://postgres:Shreya!!03@db.shdqhvelidojpnyuzsyk.supabase.co:5432/postgres"
 
-DATABASE_URL = os.getenv("DATABASE_URL", RENDER_DATABASE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL", SUPA_DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 
